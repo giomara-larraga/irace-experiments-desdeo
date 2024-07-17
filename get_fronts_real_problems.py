b97a7ac9bee0be3f075a7239c0e557dc65cd1ea3
giomara-larraga <giomara.lm@gmail.com>
2024-07-17 16:36:52 +0300

    changes to run experiments

--- get_fronts_real_problems.py
+++ get_fronts_real_problems.py
@@ -1,39 +1,36 @@
+
 from CarSideImpact import car_side_impact
-from desdeo_problem.testproblems.MultipleClutchBrakes import multiple_clutch_brakes
-from desdeo_problem.testproblems.RiverPollution import river_pollution_problem
-from desdeo_problem.testproblems.VehicleCrashworthiness import vehicle_crashworthiness
+from RiverPollutionProblem import river_pollution_problem
+from VehicleCrashWorthiness import vehicle_crashworthiness
 
-from desdeo_emo.EAs.IBEA import IBEA
-from desdeo_emo.EAs.AutoNSGAIII import AutoNSGAIII
+from desdeo_emo.EAs.AutoIBEA import AutoPBEA
 
-
-problems = [car_side_impact(), multiple_clutch_brakes(), river_pollution_problem(), vehicle_crashworthiness()]
-objectiver = [3, 3, 4, 4]
+problems = [car_side_impact(three_obj=False), river_pollution_problem(), vehicle_crashworthiness()]
 #algorithms = [IBEA, RVEA]
 
 def compute_front (problem):
-    evolver = IBEA(
-    problem,
-    n_iterations=1,
-    n_gen_per_iter=300,
-    population_size=100,
+    evolver = AutoPBEA(
+        problem,
+        n_iterations=1,
+        n_gen_per_iter=300,
+        population_size=100,
+        crossover_probability = 0.0318,
+        crossover_distribution_index=0.5,
+        mutation_probability = 0.0328,
+        polinomial_mut_dist_index  = 140.3278,
     )
-    evolver.start()
+
+    #evolver.set_interaction_type("Reference point")
+
     while evolver.continue_evolution():
         evolver.iterate()
-        print(f"Running iteration {evolver._iteration_counter}")
-    #print(f"Number of non-dominated solutions: {len(evolver.non_dominated['objectives'])}")
 
-    #pref, plot = evolver.iterate(pref)
+    objectives = evolver.population.objectives
+    ideal = evolver.population.problem.ideal
 
-
-    # obj = evolver.non_dominated["objectives"]
-    _, obj, _ = evolver.end()
-    #print(objective_values)
-    # show some of the ibea pop somehow
-    print("IBEA ideal",evolver.population.problem.ideal)
+    print(ideal)
     return 0
 
 
 if __name__== "__main__":
-    compute_front(problems[3])
+    compute_front(problems[1])
